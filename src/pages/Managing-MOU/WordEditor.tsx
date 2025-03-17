607792f26ed27b78c3857a4125141f0279e1697f
LuAn <63506861+ND-Luan@users.noreply.github.com>
2025-03-17 16:25:50 +0700

    update

--- src/pages/Managing-MOU/WordEditor.tsx
+++ src/pages/Managing-MOU/WordEditor.tsx
@@ -1,16 +1,45 @@
-import React from "react";
-import GgLogin from "../../components/app-layout/components/GgLogin";
-import { GoogleOAuthProvider } from '@react-oauth/google'; 
-
-const clientId = "393569771469-alife8pccujdsd3hk4nmnv1ben8npnhi.apps.googleusercontent.com";
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 
 const WordEditor: React.FC = () => {
+    const [value, setValue] = useState('');
+
+    const modules = {
+        toolbar: [
+            [{ 'font': [] }],
+            [{ 'size': ['small', false, 'large', 'huge'] }],
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            ['bold', 'italic', 'underline', 'strike'],
+            [{ 'color': [] }, { 'background': [] }],
+            [{ 'align': [] }],
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [{ 'indent': '-1'}, { 'indent': '+1' }],
+            [{ 'direction': 'rtl' }],
+            ['blockquote', 'code-block'],
+            ['link', 'image', 'video'],
+            ['clean']
+        ]
+    };
+
+    const formats = [
+        'font', 'size', 'header',
+        'bold', 'italic', 'underline', 'strike',
+        'color', 'background',
+        'align', 'direction', 'indent',
+        'list',
+        'blockquote', 'code-block',
+        'link', 'image', 'video'
+    ];
+
     return (
-        <div>
-            <GoogleOAuthProvider clientId={clientId}>
-                <GgLogin />
-            </GoogleOAuthProvider>
-        </div>
+        <ReactQuill 
+            theme="snow" 
+            value={value} 
+            onChange={setValue}
+            modules={modules}
+            formats={formats}
+        />
     );
 };
 
